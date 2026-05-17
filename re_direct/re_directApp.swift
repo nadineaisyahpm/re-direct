a83fbb6e98d1a7e189aac8b39f9140145c3d14fb
Nadine Aisyah Putri Maharani <nadineaisyah170806@gmail.com>
2026-05-17 20:03:22 +0700

    Initial Commit

---
+++ re_direct/re_directApp.swift
@@ -0,0 +1,32 @@
+//
+//  re_directApp.swift
+//  re_direct
+//
+//  Created by nadine on 17/5/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct re_directApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
